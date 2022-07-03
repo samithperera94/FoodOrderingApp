71e1514f6764b063d9274b61372294e7c54267b7
samithperera94 <samiththilanka94@gmail.com>
2022-07-04 02:22:21 +0530

    header component UI bug fixed

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,18 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 
-const HeaderCartButton = () => {
-    return
-    <button className={classes.button}>
-        <span className={classes.icon}>
-            <CartIcon />
+const HeaderCartButton = (props) => {
+    return (
+        <button className={classes.button}>
+            <span className={classes.icon}>
+                <CartIcon />
+            </span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>
+                3
         </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>
-            3
-        </span>
-    </button>
-
+        </button>
+    );
 };
 
 export default HeaderCartButton;
